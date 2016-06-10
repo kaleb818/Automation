c60848959921ccc0e419fe3df316c817dd616a3d
kaleb818 <jonathan.bus@spr.com>
2016-06-10 11:03:06 -0500

    added reboot script

---
+++ reboot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SERVERS=( 10.4.0.101 10.4.0.102 10.4.0.103 10.4.0.104 10.4.0.105 10.4.0.106 10.4.0.107 )
+
+for HOST in ${SERVERS[@]}; do
+
+	ssh root@${HOST} 'reboot'
+
+    if [[ $? -ne 0 ]]; then
+        echo "ERROR: $HOST did not complete"
+     else
+        echo "$HOST complete"
+    fi
+done
