1d7e788b9f68ca186c182317afeb2ba1e22f7cb9
kaleb818 <jonathan.bus@spr.com>
2016-06-13 18:25:19 -0500

    Added Ansible setup for RH

---
+++ setupAnsRH.sh
@@ -0,0 +1,9 @@
+yum update
+yum install git -y
+git clone git://github.com/ansible/ansible.git --recursive
+cd ./ansible/
+source .ansible/hacking/env-setup
+ansible/easy_install pip
+pip install paramiko PyYAML Jinja2 httplib2 six
+rpm -iUvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+sudo yum install ansible
