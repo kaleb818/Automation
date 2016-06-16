5aebb3b70d5f3c2f06965b0c0de95f9aed2bbba5
kaleb818 <jonathan.bus@spr.com>
2016-06-16 13:14:52 -0500

    Clean up

--- ansible-cloudera-hadoop-master/roles/hadoop/templates/bin/hdfs-ready.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ready_filename="/hdfs_ready"
-sudo -Hu hdfs timeout 10 hdfs dfs -touchz $ready_filename &>/dev/null && exit 0
-exit 1
-
--- updateRH.sh
+++
@@ -1,7 +0,0 @@
-#disable firewall
-service iptables save
-service iptables stop
-chkconfig iptables off
-
-#set root pw
-echo "SprPassw0rd123" | passwd root --stdin
