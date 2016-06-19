ff6205fa2dcdb688e8fc30b02818b21f72c4f120
kaleb818 <jonathan.bus@spr.com>
2016-06-19 16:17:39 -0500

    Updated setupNode.sh

--- scripts/setupNode.sh
+++ scripts/setupNode.sh
@@ -20,11 +20,9 @@ else
   echo "jbus ALL=(ALL) NOPASSWD: ALL" >> $1
 fi
 
-#create the .ssh directory for passwordless key access
-mkdir /home/jbus/.ssh
-
 #copy the public key for passwordless access
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAo66zeO/xoY5rybnpcpMST10HdZTdp39YMp1SvbwWAlY0hHCKCmXtllGwkrIrGbRiDgTXJOEh8/1/IhxsjWDlIUosGdWaC5zXrWjXlGPcoGPqi$' | cat >> /home/jbus/.ssh/authorized_keys
 
 #set the permissions on the directories
+echo "changing permissions on .ssh"
 chmod 700 .ssh; chmod 640 .ssh/authorized_keys
