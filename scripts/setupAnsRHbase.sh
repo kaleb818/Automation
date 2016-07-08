7ba2f0abe2ba208c52e685ea8703240fab7ff7dc
kaleb818 <jonathan.bus@spr.com>
2016-07-07 21:04:31 -0500

    Added base RHEL Ansible Script

---
+++ scripts/setupAnsRHbase.sh
@@ -0,0 +1,8 @@
+yum update -y
+yum install git -y
+git clone git://github.com/ansible/ansible.git /home/jbus/ansible --recursive 
+source /home/jbus/ansible/hacking/env-setup
+easy_install pip
+pip install paramiko PyYAML Jinja2 httplib2 six
+rpm -iUvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+sudo yum install ansible -y
