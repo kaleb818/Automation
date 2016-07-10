dc60f8dee9338ddbc9421ba96283a4faaec345e6
kaleb818 <jonathan.bus@spr.com>
2016-07-09 20:34:04 -0500

    .

--- scripts/setupAnsRHbase.sh
+++ scripts/setupAnsRHbase.sh
@@ -1,8 +1,8 @@
-sudo yum update -y
-sudo yum install git -y
+yum update -y
+yum install git -y
 git clone git://github.com/ansible/ansible.git /home/jbus/ansible --recursive 
-sudo source /home/jbus/ansible/hacking/env-setup
-sudo easy_install pip
-sudo pip install paramiko PyYAML Jinja2 httplib2 six
-sudo rpm -iUvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-sudo yum install ansible -y
+source /home/jbus/ansible/hacking/env-setup
+easy_install pip
+pip install paramiko PyYAML Jinja2 httplib2 six
+rpm -iUvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+yum install ansible -y
